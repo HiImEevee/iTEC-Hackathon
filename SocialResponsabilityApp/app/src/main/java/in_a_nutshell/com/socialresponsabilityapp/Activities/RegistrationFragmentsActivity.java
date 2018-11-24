38552dacc96f7899df8b205e17d47f9cca9fc116
Matei Andrei <andrei.matei2010@gmail.com>
2018-11-24 19:06:01 +0200

    Starting registration

--- SocialResponsabilityApp/app/src/main/java/in_a_nutshell/com/socialresponsabilityapp/Activities/RegistrationFragmentsActivity.java
+++ SocialResponsabilityApp/app/src/main/java/in_a_nutshell/com/socialresponsabilityapp/Activities/RegistrationFragmentsActivity.java
@@ -1,15 +1,38 @@
 package in_a_nutshell.com.socialresponsabilityapp.Activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewParent;
 
+import in_a_nutshell.com.socialresponsabilityapp.Adapters.SectionsPageAdapter;
+import in_a_nutshell.com.socialresponsabilityapp.Fragments.RegistrationFragments.AgeFragment;
+import in_a_nutshell.com.socialresponsabilityapp.Fragments.RegistrationFragments.GenderFragment;
 import in_a_nutshell.com.socialresponsabilityapp.R;
 
 public class RegistrationFragmentsActivity extends AppCompatActivity {
+
+    //Widgets
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_fragments);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AgeFragment());
+        adapter.addFragment(new GenderFragment());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ SocialResponsabilityApp/app/src/main/java/in_a_nutshell/com/socialresponsabilityapp/Fragments/RegistrationFragments/AgeFragment.java
@@ -0,0 +1,118 @@
+package in_a_nutshell.com.socialresponsabilityapp.Fragments.RegistrationFragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import in_a_nutshell.com.socialresponsabilityapp.R;
+
+
+public class AgeFragment extends Fragment {
+
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    private String mParam1;
+//    private String mParam2;
+//
+//    private OnFragmentInteractionListener mListener;
+
+    private SeekBar ageSeekBar;
+    private TextView ageTextView;
+
+    public AgeFragment() {
+        // Required empty public constructor
+    }
+
+//    public static AgeFragment newInstance(String param1, String param2) {
+//        AgeFragment fragment = new AgeFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_age, container, false);
+
+        ageSeekBar = view.findViewById(R.id.ageSeekBar);
+        ageTextView = view.findViewById(R.id.ageTV);
+
+        ageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                ageTextView.setX((seekBar.getWidth() - ageTextView.getWidth()) / 100 * i);
+                ageTextView.setText(i + " years");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        return view;
+
+    }
+//
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+
+    /*@Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }*/
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
